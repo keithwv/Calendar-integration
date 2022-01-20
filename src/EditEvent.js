4901c299b8ddbdca51f8593d8d77d7e0e134f7c3
shubhambhawsar47b <shubham.bhawsar@47billion.com>
2022-01-20 16:12:34 +0530

    feat:add edit functionality

--- src/EditEvent.js
+++ src/EditEvent.js
@@ -3,10 +3,8 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'react
 import { useFormik } from 'formik';
 import * as Yup from "yup";
 
-const AddEvent = (props) => {
-    // console.log("Props---------->", props)
+const EditEvent = (props) => {
     const [modal, setModal] = useState(true);
-    // const [value, setValue] = useState("");
 
     const formik = useFormik({
         initialValues: {
@@ -25,7 +23,7 @@ const AddEvent = (props) => {
 
     const toggle = () => {
         setModal(!modal);
-        props.method()
+        // props.method()
 
     }
 
@@ -33,32 +31,30 @@ const AddEvent = (props) => {
     // const createEventId = () => {
     //     return String(eventGuid++)
     // }
+    // const handleDateSelect = () => {
+    //     // console.log("Props---------->", props.data.data.view.calendar)
+    //     let calendarApi = props.data.data.view.calendar
+    //     calendarApi.unselect()
+    //     let title = formik.values.title
+    //     let description = formik.values.description
+    //     if (title) {
+    //         calendarApi.addEvent({
+    //             id: createEventId(),
+    //             title,
+    //             description,
+    //             start: props.data.data.startStr,
+    //             end: props.data.data.endStr,
+    //             allDay: props.data.data.allDay
+    //         })
+    //     }
+    //     props.data.check = false
 
-
-    const handleDateSelect = () => {
-        let calendarApi = props.data.data.view.calendar
-
-        calendarApi.unselect()
-        let title = formik.values.title
-        let description = formik.values.description
-        if (title) {
-            calendarApi.addEvent({
-                id: Date.now(),
-                title,
-                description,
-                start: props.data.data.startStr,
-                end: props.data.data.endStr,
-                allDay: props.data.data.allDay
-            })
-        }
-        props.data.check = false
-
-        setModal(!modal);
-    }
+    //     setModal(!modal);
+    // }
     return (
         <div>
             <Modal isOpen={modal} toggle={() => toggle()}>
-                <ModalHeader toggle={() => toggle()}>Please add Title and Description for your Event</ModalHeader>
+                <ModalHeader toggle={() => toggle()}>Edit Title and Description for your Event</ModalHeader>
                 <ModalBody>
                     <Input type="text" name="title" id="title" value={formik.values.title} onChange={formik.handleChange} placeholder='Add title' /><br></br>
                     {formik.errors.title ? <p>{formik.errors.title}</p> : null}
@@ -66,7 +62,7 @@ const AddEvent = (props) => {
                     {formik.errors.description ? <p>{formik.errors.description}</p> : null}
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={() => handleDateSelect()}>Ok</Button>{' '}
+                    {/* <Button color="primary" onClick={() => handleDateSelect()}>Ok</Button>{' '} */}
                     <Button color="secondary" onClick={() => toggle()}>Cancel</Button>
                 </ModalFooter>
             </Modal>
@@ -74,4 +70,4 @@ const AddEvent = (props) => {
     )
 }
 
-export default AddEvent
+export default EditEvent
--- src/RemoveEvent.js
+++ src/RemoveEvent.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
 import { FaTrash, FaEdit } from "react-icons/fa";
+// import EditEvent from './EditEvent';
 
 const RemoveEvent = (props) => {
     const [modal, setModal] = useState(true);
+    // const [editEvent, setEditEvent] = useState(false);
+    const [disabled, setDisabled] = useState(true);
+    const [data, setData] = useState(props.data.data.event.title)
+    const [dataDC, setDataDC] = useState(props.data.data.event._def.extendedProps.description)
+
+
     const toggle = () => {
         setModal(!modal);
         props.method()
@@ -14,8 +21,28 @@ const RemoveEvent = (props) => {
         props.data.data.event.remove()
         props.method()
     }
+
+    const editFunc = () => {
+        // setEditEvent(true)
+        // props.method()
+        setDisabled(!disabled);
+    }
+
+    // console.log("Props----Description", props.data.data.event._def.extendedProps.description)
+
     let date = new Date(props.data.data.event.start)
     let today = date.toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' })
+
+    const handleDateSelect = () => {
+        let calendarApi = props.data.data.view.calendar
+        // console.log("CA", props.data.data)
+        let currentEvent = calendarApi.getEventById(props.data.data.event.id);
+        currentEvent._def.title = data
+        currentEvent._def.extendedProps = { description: dataDC }
+        props.data.check = false
+        toggle()
+    }
+
 
     return (
         <div>
@@ -23,19 +50,21 @@ const RemoveEvent = (props) => {
                 <ModalHeader toggle={() => toggle()} style={{ fontSize: "10px" }}>
                     <div>Remove Event from the Calendar
                         <FaTrash style={{ marginRight: "10px", marginLeft: "10px" }} onClick={() => handleEventClick()} />
-                        <FaEdit />
+                        <FaEdit onClick={() => editFunc()} />
                     </div>
                 </ModalHeader>
                 <ModalBody>
-                    <b>{props.data.data.event.title}</b><br></br>
                     <i>{today}</i>
+                    <Input className='mt-2' type="text" value={data} disabled={disabled} onChange={(e) => setData(e.target.value)} />
+                    <Input className='mt-2' type="text" value={dataDC} disabled={disabled} onChange={(e) => setDataDC(e.target.value)} />
+
                 </ModalBody>
                 <ModalFooter>
-                    {/* <Button color="danger" onClick={() => handleEventClick()}>Delete</Button>{' '} */}
-                    {/* <Button color="primary" onClick={() => toggle()}>Ok</Button>{' '}
-                    <Button color="secondary" onClick={() => toggle()}>Cancel</Button> */}
+                    <Button color="primary" onClick={() => handleDateSelect()}>Ok</Button>{' '}
+                    <Button color="secondary" onClick={() => toggle()}>Cancel</Button>
                 </ModalFooter>
             </Modal>
+            {/* {editEvent ? <EditEvent data={editEvent} /> : null} */}
         </div>
     )
 }
